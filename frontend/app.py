6b523b01b6474b98b1374093d926719f21f72f64
Varun Swaminathan <85042815+flerovious@users.noreply.github.com>
2023-12-04 20:51:44 -0800

    Add answer options column

--- frontend/app.py
+++ frontend/app.py
@@ -6,6 +6,17 @@ class Expertise(Enum):
     INTRODUCTORY_CHEMISTRY = "Introductory Chemistry 🧪"
 
 
+class InputField(Enum):
+    QUESTION = "question"
+    OPTIONS = "option"
+    ANSWER = "answer"
+
+
+class OutputField(Enum):
+    GUIDE = "guide"
+    CITATIONS = "citations"
+
+
 st.set_page_config(
     page_title="Tango AI",
     page_icon="💃",
@@ -21,43 +32,59 @@ st.markdown(
 )
 
 # Initialize session state
-if "question" not in st.session_state:
-    st.session_state["question"] = ""
-if "answer" not in st.session_state:
-    st.session_state["answer"] = ""
-if "citations" not in st.session_state:
+for field in InputField:
+    if field not in st.session_state:
+        st.session_state[field] = ""
+
+# uncomment this when citations and guiding questions are generated (work for integrating with backend)
+# for field in OutputField:
+#     if field not in st.session_state:
+#         st.session_state[field] = ""
+
+if OutputField.CITATIONS not in st.session_state:
     # replace this with empty string and add citations as they are generated on button click
-    st.session_state["citations"] = "[1] Citation placeholder"
-if "guide" not in st.session_state:
+    st.session_state[OutputField.CITATIONS] = "[1] Citation placeholder"
+if OutputField.GUIDE not in st.session_state:
     # replace this with empty string and add guiding questions as they are generated on button click
-    st.session_state["guide"] = "Guiding questions will appear here"
+    st.session_state[OutputField.GUIDE] = "Guiding questions will appear here"
 
 input_validation = st.empty()
-col1, col2 = st.columns(2)
+col1, col2, col3 = st.columns(3)
 
 with col1:
-    st.session_state["question"] = st.text_area(
+    st.session_state[InputField.QUESTION] = st.text_area(
         "**Question**",
-        value=st.session_state["question"],
-        placeholder="Enter the full question with possible options",
+        value=st.session_state[InputField.QUESTION],
+        placeholder="Enter the full question",
     ).strip()
 
 with col2:
-    st.session_state["answer"] = st.text_area(
+    st.session_state[InputField.OPTIONS] = st.text_area(
+        "**Answer Options**",
+        value=st.session_state[InputField.OPTIONS],
+        placeholder="Enter possible answer options",
+    ).strip()
+
+with col3:
+    st.session_state[InputField.ANSWER] = st.text_area(
         "**Answer**",
-        value=st.session_state["answer"],
+        value=st.session_state[InputField.ANSWER],
         placeholder="Enter the correct answer",
     ).strip()
 
 if st.button("✏️ Generate Guiding Questions"):
-    if not st.session_state["question"] or not st.session_state["answer"]:
-        input_validation.warning("Question and answer cannot be empty")
+    if not st.session_state[InputField.QUESTION]:
+        input_validation.warning("Question cannot be empty")
+    elif not st.session_state[InputField.OPTIONS]:
+        input_validation.warning("Answer options cannot be empty")
+    elif not st.session_state[InputField.ANSWER]:
+        input_validation.warning("Answer cannot be empty")
     else:
         st.write("## 📚 Guiding Questions")
-        if "guide" in st.session_state:
-            st.caption(st.session_state["guide"])
+        if OutputField.GUIDE in st.session_state:
+            st.caption(st.session_state[OutputField.GUIDE])
 
         # Show citations in accordion
         with st.expander("See citations"):
-            if "citations" in st.session_state:
-                st.caption(st.session_state["citations"])
+            if OutputField.CITATIONS in st.session_state:
+                st.caption(st.session_state[OutputField.CITATIONS])
